cbddc3d633ab8a9a073d6fee3223f0e7123808a1
Oscar A. Rodriguez <52334508+AndresKenji@users.noreply.github.com>
2025-05-31 14:42:18 -0500

    fix: se corrigen las url

--- src/render.py
+++ src/render.py
@@ -1,10 +1,24 @@
+import shutil
+from pathlib import Path
 from jinja2 import Environment, FileSystemLoader
 from src.models import Resume
-from pathlib import Path
 
 def render_resume(resume: Resume, output_path="docs/index.html"):
     env = Environment(loader=FileSystemLoader("src/templates"))
     template = env.get_template("layout.html")
     rendered_html = template.render(resume=resume)
+
+    # Escribe el HTML
     Path(output_path).write_text(rendered_html)
+
+    # Copiar el CSS
+    css_src = Path("src/static/css/styles.css")
+    css_dst = Path("docs/static/css/styles.css")
+    css_dst.parent.mkdir(parents=True, exist_ok=True)
+    shutil.copy(css_src, css_dst)
+    # Copiar imágenes
+    img_src = Path("src/static/img")
+    img_dst = Path("docs/static/img")
+    if img_src.exists():
+        shutil.copytree(img_src, img_dst, dirs_exist_ok=True)
 
