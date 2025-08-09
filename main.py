9a84e075b91b671e639371d79243876fe918a503
Oscar A. Rodriguez <52334508+AndresKenji@users.noreply.github.com>
2025-08-09 12:36:36 -0500

    agregando logging

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+import logging
 import json
-from src.models import Resume
+
 from pathlib import Path
+
+from src.logging_config import setup_logger
+from src.models import Resume
 from src.render import render_resume
+
+
+logger: logging.Logger = setup_logger(
+    name="cv_creator",
+    level=logging.INFO,
+    format_string='%(levelname)s - %(asctime)s - %(name)s - %(message)s',
+    include_file_handler=False,
+)
 
 def load_resume(path: str = "src/data/resume.json") -> Resume:
     data = json.loads(Path(path).read_text())
